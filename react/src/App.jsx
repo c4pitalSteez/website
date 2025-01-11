2692d94d91be9d5b043c7dd8b6692448f7acb0ae
c4pitalSteez <willgreenwood04@gmail.com>
2025-01-11 04:22:10 -0500

    lighting

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -7,9 +7,9 @@ import './App.css'
 function App() {
   return (
     <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
+      <ambientLight color='blue' intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.25} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <pointLight position={[0, 0, 0]} decay={0} intensity={Math.PI} />
       <BoxGrid />
     </Canvas>
   )
